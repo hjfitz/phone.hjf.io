83d9064ec40271dc2729bea8642a42b777225994
Harry Fitzgerald <harry.f@elanco.com>
2021-01-21 14:15:56 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Peer to Peer Phone Calling with Peer.js
+
+Playing with Snowpack, React and WebRTC. 
+
+To get going, install dependencies (`yarn`) and run `yarn dev`.
+
+To call somebody, get them to go to the same page and get them to send you their ID!
+
+Very basic implementation with PeerJS in ~70 lines of JavaScript. 
+
+I'd like to:
+* Do some cool facial recognition stuff
+* Implement cutsom backgrounds
+* Add mute/unmute
+* Support multiple peers
+
+
---
+++ main.jsx
@@ -0,0 +1,66 @@
+import React, {useEffect, useRef, useState} from 'react'
+import {render} from 'react-dom'
+import Peer from 'peerjs'
+
+const entry = document.getElementById('main')
+
+const randID = () => Math.random().toString(36).substr(2, 10)
+
+const App = () => {
+	const idInit = randID()
+	const id = useRef(idInit)
+	const peer = useRef(new Peer(idInit))
+	const [stream, setStream] = useState(null)
+
+	console.log({peer})
+
+	const video = useRef(null)
+	const them = useRef(null)
+	const idIn = useRef(null)
+
+	function call() {
+		const dial = peer.current.call(idIn.current.value, stream)
+		dial.on('stream', (remote) => {
+			them.current.srcObject = remote
+			them.current.play()
+		})
+	}
+
+	useEffect(() => {
+		if (!stream) {
+			navigator.mediaDevices.getUserMedia({video: true}).then((s) => setStream(s))
+		} else {
+			video.current.srcObject = stream
+			video.current.play()
+			peer.current.on('call', (dial) => {
+				dial.answer(stream) // Answer the call with an A/V stream.
+				dial.on('stream', (remote) => {
+					them.current.srcObject = remote
+					them.current.play()
+				})
+			})
+		}
+	}, [stream])
+
+	return (
+		<main>
+			<div>
+				<h1>Shitty video client</h1>
+				<p><strong>Your ID:</strong> {id.current}</p>
+				<label>
+					Enter partner ID
+					<input type="text" ref={idIn} />
+				</label>
+				<button type="button" onClick={call}>Call</button>
+			</div>
+			<div>
+				<video ref={video} />
+			</div>
+			<div>
+				<video ref={them} />
+			</div>
+		</main>
+	)
+}
+
+render(<App />, entry)
